74fe5727e88e5cc378fe5e34a25dd26dbd9c6d3f
League <League@AdminisorsiMac2.attlocal.net>
2018-06-20 12:01:54 -0700

    d3

--- src/section3/OwnAdventure.java
+++ src/section3/OwnAdventure.java
@@ -22,6 +22,7 @@ public class OwnAdventure {
 				int confirmBear = JOptionPane.showConfirmDialog(null, "Are you sure?");
 				if (confirmBear == 0) {
 					JOptionPane.showMessageDialog(null, "The huge bear wakes up and immediately swallows you whole.  You die.");
+					choice1 = 5;
 				}
 				else if(confirmBear == 1) {
 					JOptionPane.showMessageDialog(null, "You grab the gold and run out of the cave. ");
@@ -30,26 +31,37 @@ public class OwnAdventure {
 				
 			if(caveChoice == 1) {
 				JOptionPane.showMessageDialog(null, "You grab the gold and run out of the cave. ");
-				int choice2 = JOptionPane.showOptionDialog(null, "Which do you want to explore?", "Explore", 0, JOptionPane.PLAIN_MESSAGE, null, new String[] {"Forest","Temple"}, args);
+				choice1 = JOptionPane.showOptionDialog(null, "Which do you want to explore?", "Explore", 0, JOptionPane.PLAIN_MESSAGE, null, new String[] {"Forest","Temple"}, args);
 			}
+			
 			}//end cave
 			
 		}//end choose cave
 		
-		if(choice1 == 1) {
+		if(choice1 == 1 || choice1 == 0) {
+			forestExplore = true;
 			JOptionPane.showMessageDialog(null, "You go into the forest and find edible berries and fresh water.  \nAfter walking a little further, you see a monkey high up in a tree.  The monkey shouts \"reu3asdfgwa\" ");
 			int getPass = JOptionPane.showConfirmDialog(null, "Do you copy?");
-			
-			if(getPass == 0 && caveExplore){
-				
+			if(getPass == 0 && caveExplore) {
+				temple();
 			}
+
 		}//end choose forest
 		
 	}//main
 	
 	static void temple() {
 		if(caveExplore && forestExplore) {
-			int sacrifice = JOptionPane.showConfirmDialog(null, "You enter the temple see a large statue.  \nThe statue's eyes suddenly begin to glow and it roars, \"Are you prepared to make a sacrifice to the guardian of the temple?\"");
+			int sacrifice = JOptionPane.showConfirmDialog(null, "You enter the temple and see a large statue.  \nThe statue's eyes suddenly begin to glow and it roars, \"Are you prepared to make a sacrifice to the guardian of the temple?\"");
+			if(sacrifice == 1) {
+				JOptionPane.showConfirmDialog(null, "The statue vaoprizes you and you die.");
+			}
+			else if (sacrifice == 0) {
+				String password = JOptionPane.showInputDialog("You place down the berries and the gold.  \nThe statue hums and opens to reveal a door.  \nWhat is the password to the door?");
+				if(password.equals("reu3asdfgwa")) {
+					
+				}
+			}
 		}
 	}
 	
